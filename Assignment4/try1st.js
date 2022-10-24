20e3c489591d684d1869a295ad14b590688008d2
JieNZhou <jie.n.zhou@mail.mcgill.ca>
2022-10-24 09:12:30 -0400

    edited and assignment4

---
+++ Assignment4/try1st.js
@@ -0,0 +1,24 @@
+    
+function tog(){
+        var x = document.getElementById("divsong1")
+        
+        if (x.style.display==="none"){
+            x.style.display ="block";
+        }
+    
+        else {
+            x.style.display= "none";
+        }
+    }
+
+function togg(){
+        var x = document.getElementById("divsong2")
+        
+        if (x.style.display==="none"){
+            x.style.display ="block";
+        }
+    
+        else {
+            x.style.display= "none";
+        }
+    }
